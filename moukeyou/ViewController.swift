512d3774157e4fe65991f7e692adc30482d836dc
tcs14049 <tcs14049@tcs-mac-1609.local>
2018-10-02 14:13:45 +0900

    スタートボタンを押した時にレジの音を鳴らすようにしました。

--- moukeyou/ViewController.swift
+++ moukeyou/ViewController.swift
@@ -11,9 +11,6 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    var player:AVAudioPlayer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,16 +21,5 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func playSound(sender: AnyObject) {
-        // 再生する音声ファイルを指定する
-        let soundURL = Bundle.main.url(forResource: "sound", withExtension: "mp3")
-        do {
-            // 効果音を鳴らす
-            player = try AVAudioPlayer(contentsOf: soundURL!)
-            player?.play()
-        } catch {
-            print("error...")
-        }
-    }
-
+    
 }
